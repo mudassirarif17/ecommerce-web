142224a9420f16c66b1c5c6308b8398fe7843c8f
mudassirarif17 <mudassirinoxent@gmail.com>
2023-09-27 10:37:14 -0700

    redux

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,15 +6,29 @@ import Filter from '../../components/filter/Filter';
 import ProductCard from '../../components/productCard/ProductCard';
 import Track from '../../components/track/track';
 import Testimonial from '../../components/testimonial/Testimonial';
-
+import { useDispatch, useSelector } from 'react-redux';
+import {addToCart , deleteFromCart} from "../../redux/cartSlice"
 const Home = () => {
   const context = useContext(myContext);
   // When state has only one property
   // const {name , rollno} = context;
 
   const {mode , toggleMode} = context;
+  const dispatch = useDispatch();
+  const cartItem = useSelector(state => state.cart);
+  console.log(cartItem);
+  const addCart = ()=>{
+    dispatch(addToCart("shirt"))
+  }
+  const deleteCart = ()=>{
+    dispatch(deleteFromCart("shirt"))
+  }
   return (
     <Layout>
+      <div className="flex justify-center">
+        <button onClick={()=>addCart()} className='bg-gray-300 mx-4'>add</button>
+        <button className='bg-gray-300 mx-4'>dlt</button>
+      </div>
       <HeroSection/>
       <Filter/>
       <ProductCard/>
